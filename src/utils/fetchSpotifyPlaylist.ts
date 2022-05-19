79f225625721d345452b120217c9cc1411d04e19
Marius Jagminas <asmarenas@gmail.com>
2022-05-19 14:12:08 +0200

    Display spotify error

--- src/utils/fetchSpotifyPlaylist.ts
+++ src/utils/fetchSpotifyPlaylist.ts
@@ -30,25 +30,28 @@ export const fetchSpotifyPlaylist = async (
   spotifyRefreshToken: string,
   spotifyCredentials: string
 ): Promise<ISpotifyPlaylist | undefined> => {
-  try {
-    const token = await getSpotifyAccessToken(
-      spotifyRefreshToken,
-      spotifyCredentials
+  const token = await getSpotifyAccessToken(
+    spotifyRefreshToken,
+    spotifyCredentials
+  );
+
+  const options = {
+    method: "GET",
+    headers: { authorization: `Bearer  ${token}` },
+  };
+
+  const response = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistId}`,
+    options
+  );
+  const playlist = await response.json();
+
+  if (!playlist.tracks) {
+    console.log("fetchSpotifyPlaylist response", playlist);
+    throw new Error(
+      "Failed to fetch playlist, check the console for the detailed error messages"
     );
-
-    const options = {
-      method: "GET",
-      headers: { authorization: `Bearer  ${token}` },
-    };
-
-    const response = await fetch(
-      `https://api.spotify.com/v1/playlists/${playlistId}`,
-      options
-    );
-    const playlist = await response.json();
-
-    return playlist;
-  } catch (error) {
-    console.log({ msg: "Error in fetchSpotifyPlaylist", error });
   }
+
+  return playlist;
 };
