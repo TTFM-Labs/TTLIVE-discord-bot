a1e35aa867d12e0389c07d1fa9f3f0c77eb9559c
Ryan O'Gorman <ryan@tt.live>
2023-01-31 11:48:39 -0700

    add config file

---
+++ config.ts
@@ -0,0 +1,8 @@
+import { IConfig } from "./src/types";
+
+export const config: IConfig = {
+  discord_token: "xxxxx", // Discord bot token https://discord.com/developers/docs/intro
+  spotify_credentials: "xxxxx", // Spotify dev credentials - convert "{client_id}:{client_secret}" to base64
+  email: "xxxx@xx", // tt.live user email
+  password: "xxxxxxxx", // tt.live user password
+};
